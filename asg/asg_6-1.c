a826dca1589b5e4d5d6fa1b8036308d9c148905d
ahmedos1999 <ah_os5@yahoo.com>
2024-01-06 22:42:56 +0200

    ASG_6-1

--- asg/asg_6-1.c
+++ asg/asg_6-1.c
@@ -7,15 +7,23 @@ int main ()
 	
 	for (i = 0; i < 5; i++)
 	{
-		printf("Enter Student No.%d Data\n\n", i + 1);
+		printf("\nEnter Student No.%d Data\n\n", i + 1);
 		printf("Enter Student No.%d Name : ",i + 1);
 		scanf(" %[^\n]s", &school[i].name);
+		while ((getchar()) != '\n' && (getchar()) != EOF)
+			continue;
 		printf("Enter Student No.%d Age : ",i + 1);
 		scanf("%d", &school[i].age);
+		while ((getchar()) != '\n' && (getchar()) != EOF)
+			continue;
 		printf("Enter Student No.%d Degree : ",i + 1);
 		scanf("%d", &school[i].degree);
+		while ((getchar()) != '\n' && (getchar()) != EOF)
+			continue;
 		printf("Enter Student No.%d Section : ",i + 1);
 		scanf("%d", &school[i].section);
+		while ((getchar()) != '\n' && (getchar()) != EOF)
+			continue;
 	}
 	print_struct(school);
 
