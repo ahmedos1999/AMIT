1e92e0a8c24b8752798fddc01c0456c289eb6d79
ahmedos1999 <ah_os5@yahoo.com>
2024-02-08 17:28:55 +0200

    Drivers

---
+++ drivers/LIBs/BIT_MATH.h
@@ -0,0 +1,14 @@
+
+ 
+#ifndef BIT_MATH_H_ 
+#define BIT_MATH_H_
+ 
+#define SET_BIT(REG,BIT_NUM)			(REG |= (1 << BIT_NUM))
+#define CLR_BIT(REG,BIT_NUM)			(REG &= (~(1 << BIT_NUM)))
+#define TOG_BIT(REG,BIT_NUM)			(REG ^= (1 << BIT_NUM))
+#define TOG_REG(REG)					(REG ^= 1)
+#define GET_BIT(REG,BIT_NUM)			((REG >> BIT_NUM) & 1)
+#define AssignNum(REG,BIT_NUM,VALUE)	(REG = ((CLR_BIT(REG,BIT_NUM)) | ((VALUE) << (BIT_NUM))))
+#define AssignReg(REG1,BIT1,REG2,BIT2)	(REG1 = ((CLR_BIT(REG1,BIT1) | (GET_BIT(REG2,BIT2) << (BIT1)))))
+ 
+#endif
